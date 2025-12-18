895e40e1a5b0c6b48072a970a500d98a25638047
AGoutieras <goutieras.anthony@gmail.com>
2025-12-18 14:08:42 +0000

    Added simple shell file and header

---
+++ super_simple_shell.c
@@ -0,0 +1,66 @@
+#include "main.h"
+
+int main(void)
+{
+	char **av, *line = NULL;
+	int i;
+	size_t len = 0;
+	ssize_t nread;
+	pid_t pid;
+
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			printf("$ ");
+
+		nread = getline(&line, &len, stdin);
+		if (nread == -1)
+			break;
+		av = line_to_av(line);
+
+		if (av[0] == NULL)
+		{
+			free(av);
+			continue;
+		}
+		if (access(av[0], X_OK) == -1)
+		{
+			perror(av[0]);
+			free(av);
+			continue;
+		}
+		pid = fork();
+		if (pid == 0)
+		{
+			execve(av[0], av, environ);
+			perror(av[0]);
+			exit(1);
+		}
+		else if (pid > 0)
+			wait(NULL);
+		else
+			perror("fork");
+		free(av);
+	}
+}
+
+char **line_to_av(char *line)
+{
+	char **av = malloc(10 * sizeof(char *));
+	char *token;
+	int i = 0;
+
+	if (!av)
+		return (NULL);
+
+	token = strtok(line, " \t\n");
+
+	while (token != NULL)
+	{
+		av[i++] = token;
+		token = strtok(NULL, " \t\n");
+	}
+	av[i] = NULL;
+
+	return (av);
+}
