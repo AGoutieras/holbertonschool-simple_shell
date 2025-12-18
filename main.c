243c2e5d246dc75d392a319ac3fcde82381f7042
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-12-18 13:13:18 +0000

    Working find function.

--- main.c
+++ main.c
@@ -16,10 +16,51 @@ int starts_with(const char *s, const char *prefix)
  * @filename: The name of the file to find
  * Return: A string of the absolute path
 */
-char *find(const char *filename)
+char *find(const char *filename, char *PATH)
 {
-	(void)filename;
-	return (NULL);
+	split_t res;
+	size_t i = 0;
+	struct stat st;
+	char *nPATH;
+
+	PATH = PATH + 5; /*Skips 'PATH='*/
+	nPATH = malloc(strlen(PATH));
+	strcpy(nPATH, PATH);
+
+	res = split(nPATH, ":");
+	
+	for (; i < res.len; ++i)
+	{
+		size_t datalen = strlen(res.data[i]);
+		char *str = malloc(datalen + 1 + strlen(filename));
+		size_t it = 0;
+		
+		while (res.data[i][it] != 0)
+		{
+			str[it] = res.data[i][it];
+			++it;
+		}
+		str[it] = '/';
+		it = 0;
+		while (filename[it] != 0)
+		{
+			str[it + datalen + 1] = filename[it];
+			++it;
+		}
+		str[datalen + it + 1] = 0;
+
+		if (stat(str, &st) != -1)
+		{
+			free(nPATH);
+			free_split(&res);
+			return str;
+		}
+		free(str);
+	}
+
+	free(nPATH);
+	free_split(&res);
+	return NULL;
 }
 
 /**
@@ -28,14 +69,14 @@ char *find(const char *filename)
  * @argv: The arguments to pass to the program
  * Return: 1 on success, 0 otherwise
 */
-int _exec(const char *filepath, char **argv)
+int _exec(const char *filepath, char **argv, char **env)
 {
 	struct stat st;
 	pid_t child;
 
-	if (stat(filepath, &st) == -1)
+	if (!filepath || stat(filepath, &st) == -1)
 	{
-		printf("File '%s' not found.\n", filepath);
+		printf("%s: no such file or directory\n", filepath);
 		return (0);
 	}
 
@@ -50,10 +91,10 @@ int _exec(const char *filepath, char **argv)
 	{
 		int err;
 
-		err = execve(filepath, argv, NULL);
+		err = execve(filepath, argv, env);
 		if (err == -1)
 		{
-			printf("Couldn't execute program.\n");
+			printf("%s: couldn't execute program.\n", filepath);
 			return (0);
 		}
 	}
@@ -66,18 +107,31 @@ int _exec(const char *filepath, char **argv)
  * main - Simple Shell, read code
  * @argc: Argument count.
  * @argv: Argument values.
+ * @env: Environment values.
  * Return: 1 on success, failure otherwise
 */
-int main(int argc, char **argv)
+int main(int argc, char **argv, char **env)
 {
+	char *PATH = NULL;
+	size_t i = 0;
 	(void)argc;
 	(void)argv;
+
+	for(; env[i] != 0; ++i)
+	{
+		if(starts_with(env[i], "PATH="))
+		{
+			PATH = env[i];
+			break;
+		}	
+	}
 
 	while (1)
 	{
 		char *line = NULL;
 		size_t size = 0;
-		split_t res;
+		split_t res = {0};
+		char* found = NULL;
 
 		printf("($) ");
 		getline(&line, &size, stdin);
@@ -88,7 +142,8 @@ int main(int argc, char **argv)
 			free_split(&res);
 			return (0);
 		}
-		_exec(res.data[0], &res.data[0]);
+		found = find(res.data[0], PATH);
+		_exec(found, &res.data[0], env);
 		free_split(&res);
 	}
 
